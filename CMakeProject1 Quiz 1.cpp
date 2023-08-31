c7341ce75f5f8ca7104f9257d6ac6997abf70587
Ryan Parks <rmp5862@psu.edu>
2023-08-31 18:11:41 -0400

    add an exe that prints my name

---
+++ CMakeProject1 Quiz 1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1 Quiz 1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1 Quiz 1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Ryan Parks" << endl;
+	return 0;
+}
